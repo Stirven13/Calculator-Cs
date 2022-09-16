678a3a447525a28f79ac1c9c2d5814fef21bb1fd
steninaa <steninaa@400D-1>
2022-09-16 12:07:24 +0700

    Create Perfect Button and also items

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,11 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace ViewModels
 {
     public class MainViewModel: ViewModelBase
     {
+
+        private string display = "display";
+
+        public string Display {
+            get => display;
+
+            set { 
+                display = value;
+                OnPropertyChanged("Display");
+            }
+        }
+
+        private string expression = "expression";
+
+
+        public string Expression
+        {
+            get => expression;
+
+            set
+            {
+                expression = value;
+                OnPropertyChanged("Expression");
+            }
+        }
 
     }
 }
